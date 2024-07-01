c673afaca7794a85326e0a2a6b0d20901becce3e
BGreenberg79 <greenbeb2@gmail.com>
2024-07-01 04:08:46 -0400

    second commit

--- 2_mastering_python_modules_and_aliases/main.py
+++ 2_mastering_python_modules_and_aliases/main.py
@@ -1,3 +1,5 @@
+#Task 1 Custom Module With Alias
+
 import text_utils as tu
 
 while True:
@@ -17,3 +19,10 @@ while True:
     else:
         print("Invalid input, reinitializing program")
         continue
+
+'''
+I start off this main file by importing the text_utils module and aliasing as it's initials tu. Then in a while loop I take inputs for a custom string as the argument we will run through our functions and transformation to decide which function to call.
+If the transformation input is capitalize I call within a print statement the capitalize_string function from the tu alias module with custom_string passed through. In an elif statement if the input is reverse I similarly call from the tu alias module the reverse_string function with custom string as argument inside a print statement.
+If neither capitalize or reverse is given as the transformation input I use an else statement to print that the input was invalid. I then use a cont_string input variable to ask the user if they would like to add another message.
+If they input "Y" we use the continue keyword, elif if they enter "N" we break, and in an else statement I print that the response was invalid and that I am reinitializing the program folllowed by the continue keyword again.
+'''
--- 2_mastering_python_modules_and_aliases/text_utils.py
+++ 2_mastering_python_modules_and_aliases/text_utils.py
@@ -7,3 +7,9 @@ def reverse_string(s):
 def capitalize_string(s):
     capital_str = s.title()
     return capital_str
+
+'''
+I define two functions we will call in the main.py file from this text_utils.py module. In reverse_string(s) for the string s we assign a new variable reversed_str which will be the string s using the slice [::-1] to
+slice through by a -1 step starting at the last character and ending with the first. This new variable is then returned from the function. In capitalize_string(s) I assign the variable capital_str which is the input string s after the built-in titlecase method has been applied.
+I then return the new capital_str string.
+'''
